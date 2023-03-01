e2559a8b1389931c11ca41d319cc9247ed9ffc4a
prpdpigithub <pikoit60@gmail.com>
2023-03-01 22:04:50 +0000

    dashboard final commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -19,5 +19,18 @@ def second_currency
   render({ :template => "currency_templates/step_two.html.erb"})
 
 end
+def convert_currency
 
+ 
+  @from_symbol = params.fetch("from_currency")
+  @to_symbol = params.fetch("to_currency")
+  @rate_data = open("https://api.exchangerate.host/convert?from#{@from_symbol}&to=#{@to_symbol}").read
+  @parsed_data = JSON.parse(@rate_data)
+  @result = @parsed_data.fetch("info").fetch("rate")
+  render({ :template => "currency_templates/step_three.html.erb"})
+
+end
+def homepage
+  render({ :template => "currency_templates/forex.html.erb"})
+end
 end
